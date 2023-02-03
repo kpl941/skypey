e62794b98e0bd07dc291af5b8b43da239466cdcf
Kapil Pateria <kapil.pateria@gmail.com>
2023-02-03 21:46:09 +0530

    Refactored the code and created User component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./containers/App";
+import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const render = () => {
+  fancyLog();
+  return ReactDOM.render(<App />, document.getElementById("root"));
+};
+render();
+store.subscribe(render);
+
+
+function fancyLog() {
+  console.log("%c Rendered with 👉 👉 👇", "background: purple; color: #FFF");
+  console.log(store.getState());
+}
